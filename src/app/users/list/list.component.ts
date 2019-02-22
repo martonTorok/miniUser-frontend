280b8501bab8105b9b0dd09c85c45284fea43975
tormartonz <tormartonz@gmail.com>
2019-02-22 15:23:59 +0100

    finalized user list component

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { UserService } from "src/app/auth/user.service";
+import { UserService } from "src/app/services/user.service";
 import { User } from "src/app/models/user.model";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: "app-list",
@@ -10,7 +11,10 @@ import { User } from "src/app/models/user.model";
 export class ListComponent implements OnInit {
   users: User[];
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.getUser();
@@ -26,4 +30,56 @@ export class ListComponent implements OnInit {
       }
     );
   }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(
+      res => {
+        this.getUser();
+        this.snackbar.openFromComponent(DeleteSuccessfulComponent, {
+          duration: 3000
+        });
+      },
+      err => {
+        this.snackbar.openFromComponent(DeleteUnSuccessfulComponent, {
+          duration: 3000
+        });
+        console.log(err);
+      }
+    );
+  }
 }
+
+@Component({
+  selector: "delete-successful-component",
+  template: `
+    <span class="deleteSuccessful">
+      Deleted user succesfully!
+    </span>
+  `,
+  styles: [
+    `
+      .deleteSuccessful {
+        font-size: 1em;
+      }
+    `
+  ]
+})
+export class DeleteSuccessfulComponent {}
+
+@Component({
+  selector: "delete-unsuccessful-component",
+  template: `
+    <span class="deleteSuccessful">
+      Deleting user was unsuccesful! Please try again.
+    </span>
+  `,
+  styles: [
+    `
+      .deleteSuccessful {
+        font-size: 1em;
+        color: red;
+      }
+    `
+  ]
+})
+export class DeleteUnSuccessfulComponent {}
