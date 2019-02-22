4e6bd3b848e144db6e7c0586dac827f3f420cb4b
tormartonz <tormartonz@gmail.com>
2019-02-22 14:34:22 +0100

    added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,55 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatButtonModule } from "@angular/material/button";
+
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
+import {
+  HomeComponent,
+  InvalidCredentialsComponent
+} from "./home/home.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { AuthService } from "./auth/auth.service";
+import { UserService } from "./auth/user.service";
+import { HttpClientModule } from "@angular/common/http";
+import { ListComponent } from './users/list/list.component';
+import { RegisterComponent } from './users/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    InvalidCredentialsComponent,
+    ListComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatSnackBarModule
+  ],
+  providers: [AuthService, UserService],
+  entryComponents: [InvalidCredentialsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
