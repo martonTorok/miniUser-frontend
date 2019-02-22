26bba30bcefbef4da845a84552a88be17fd14722
tormartonz <tormartonz@gmail.com>
2019-02-22 15:21:48 +0100

    added routes with authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ListComponent } from "./users/list/list.component";
 import { RegisterComponent } from "./users/register/register.component";
+import { AuthGuardService } from "./services/authguard.service";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "users", component: ListComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "users", component: ListComponent, canActivate: [AuthGuardService] },
+  {
+    path: "register",
+    component: RegisterComponent,
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
